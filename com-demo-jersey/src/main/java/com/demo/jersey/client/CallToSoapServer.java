42a80c1bf20da7173ae28b780b8af742f46c1822
congdanh910 <congdanh910@gmail.com>
2015-07-22 13:28:15 +0700

    Adding the code for demo client/server with Jersey

---
+++ com-demo-jersey/src/main/java/com/demo/jersey/client/CallToSoapServer.java
@@ -0,0 +1,15 @@
+package com.demo.jersey.client;
+
+import com.sun.jersey.api.client.config.ClientConfig;
+import com.sun.jersey.api.client.config.DefaultClientConfig;
+
+/**
+ * Created by danh.ho on 22/07/2015.
+ */
+public class CallToSoapServer {
+    public static  void main(String[] args){
+
+
+    }
+}
+
